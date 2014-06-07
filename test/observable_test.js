2d27f403a17f25d545afbe55224620994b6a989d
Takashi Yagita <yagita.takashi@gmail.com>
2014-06-07 16:45:26 +0900

    add test log method

--- test/observable_test.js
+++ test/observable_test.js
@@ -1,6 +1,9 @@
 /*jslint indent: 2, onevar: false*/
 /*globals TestCase, assertEquals, tddjs*/
 TestCase("Observable.addObserver", {
+	setUp: function() {
+		log("setUp");
+	},
   "test should store function": function () {
     var observable = new tddjs.util.Observable();
     var observer = function () {};
@@ -10,3 +13,7 @@ TestCase("Observable.addObserver", {
     assertEquals(observer, observable.observers[0]);
   }
 });
+
+function log(msg) {
+		jstestdriver.console.log(msg);
+}
